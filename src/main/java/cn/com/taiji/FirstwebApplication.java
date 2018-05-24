f3c7c93f269226021bc5562dd1a452e78d10dda0
xchaigl@gmail.com <xchaigl@gmail.com>
2018-05-24 11:40:15 +0800

    修改显示bean的调用方式 by xuconghui 20180524

--- src/main/java/cn/com/taiji/FirstwebApplication.java
+++ src/main/java/cn/com/taiji/FirstwebApplication.java
@@ -1,7 +1,9 @@
 package cn.com.taiji;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,27 +13,43 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class FirstwebApplication {
 
-	@Bean 
-	ApplicationRunner runner() {
-		return new ApplicationRunner() {
-			@Override
-			public void run(ApplicationArguments args) throws Exception {
-				System.out.println("Hello SpringBoot!");
-			}
-		};
-	}
+	@Autowired
+	 ApplicationContext context;
+	
+	 @Bean
+	 public CommandLineRunner runner() {
+	  return (args) -> {
+	   int count = context.getBeanDefinitionCount();
+	         System.out.println("Beans的个数：" + count);
+	         String[] names = context.getBeanDefinitionNames();
+	         for (String name : names) {
+	           System.out.printf("%s -> %s\n", name, context.getBean(name));
+	         }
+	  };
+	 }
+//	@Bean 
+//	ApplicationRunner runner1() {
+//		return new ApplicationRunner() {
+//			@Override
+//			public void run(ApplicationArguments args) throws Exception {
+//				System.out.println("Hello SpringBoot!");
+//				beanmess();
+//			}
+//			public void beanmess() {
+//		        int count = context.getBeanDefinitionCount();
+//		        System.out.println("Beans的个数：" + count);
+//		        String[] names = context.getBeanDefinitionNames();
+//		        for (String name : names) {
+//		               System.out.print(name);
+//		               System.out.print("->");
+//		               Object bean = context.getBean(name);
+//		               System.out.println(bean.getClass().getName());
+//		        }
+//			}
+//		};
+//	}
 	
 	public static void main(String[] args) {
-		//SpringApplication.run(FirstwebApplication.class, args);
-		ApplicationContext context = SpringApplication.run(FirstwebApplication.class, args);
-        int count = context.getBeanDefinitionCount();
-        System.out.println("Beans的个数：" + count);
-        String[] names = context.getBeanDefinitionNames();
-        for (String name : names) {
-               System.out.print(name);
-               System.out.print("->");
-               Object bean = context.getBean(name);
-               System.out.println(bean.getClass().getName());
-        }
+		SpringApplication.run(FirstwebApplication.class, args);
 	}
 }
